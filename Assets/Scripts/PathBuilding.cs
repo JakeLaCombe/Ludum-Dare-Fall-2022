d18304b088070cff8b9f668663306ae60338c15a
Jake LaCombe <jake.lacombe2@gmail.com>
2022-10-01 18:57:17 -0500

    Fix Issue with Path Building Bugs

--- Assets/Scripts/PathBuilding.cs
+++ Assets/Scripts/PathBuilding.cs
@@ -17,50 +17,43 @@ public class PathBuilding : MonoBehaviour
     void Start()
     {
         playerInput = GetComponent<IInputable>();
+
         GameObject startingPoint = GameObject.Find("StartingPoint");
-        TileBase tile = possiblePath.GetTile(Vector3Int.FloorToInt(startingPoint.transform.position));
-        Debug.Log(tile);
-
         playerPath.SetTile(Vector3Int.FloorToInt(startingPoint.transform.position), basePathTile);
-        possiblePath.SetTile(Vector3Int.FloorToInt(startingPoint.transform.position), basePathTile);
-
-        currentLocation = Vector3Int.FloorToInt(startingPoint.transform.position);
+        currentLocation = Vector3Int.FloorToInt(playerPath.WorldToCell(startingPoint.transform.position));
     }
 
     // Update is called once per frame
     void Update()
     {
         bool updateTiles = true;
+        Vector3Int nextLocation = new Vector3Int(currentLocation.x, currentLocation.y, currentLocation.z);
 
         if (playerInput.Left())
         {
-            currentLocation.x -= 1;
+            nextLocation.x -= 1;
         }
         else if (playerInput.Right())
         {
-            currentLocation.x += 1;
+            nextLocation.x += 1;
         }
         else if (playerInput.Up())
         {
-            currentLocation.y += 1;
+            nextLocation.y += 1;
         }
         else if (playerInput.Down())
         {
-            currentLocation.y -= 1;
+            nextLocation.y -= 1;
         }
         else
         {
             updateTiles = false;
         }
 
-        if (updateTiles)
-        {
-            Debug.Log(possiblePath.GetTile(currentLocation));
-
-            if (possiblePath.GetTile(currentLocation) != null)
-            {
-                playerPath.SetTile(currentLocation, basePathTile);
-            }
+        if (updateTiles && possiblePath.GetTile(nextLocation) != null && playerPath.GetTile(nextLocation) != basePathTile)
+        {         
+            currentLocation = nextLocation;
+            playerPath.SetTile(currentLocation, basePathTile);
         }
     }
 }
